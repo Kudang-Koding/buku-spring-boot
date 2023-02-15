b4e1e9cc395915aaa2a320cf3f536fa3e9e18753
trikhaqiqi <trikhaqiqi@gmail.com>
2023-02-15 15:12:32 +0700

    modify configuration for db

--- src/main/java/kudangkoding/gamifikasi/GamifikasiApplication.java
+++ src/main/java/kudangkoding/gamifikasi/GamifikasiApplication.java
@@ -1,13 +1,25 @@
 package kudangkoding.gamifikasi;
 
+import kudangkoding.gamifikasi.configs.AppProperties;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
+@EnableScheduling
 public class GamifikasiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamifikasiApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelmapper() {
+		return new ModelMapper();
 	}
 
 }
